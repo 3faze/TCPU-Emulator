370b41f70fae15ecc389bb5f3ea22ab4b1c80923
3faze <93428357+3faze@users.noreply.github.com>
2023-10-14 13:14:46 +0100

    Add files via upload

--- handler.py
+++ handler.py
@@ -20,13 +20,13 @@ sp = Register("SP", "16 bits")
 pc = Register("PC", "16 bits")
 
 registers = {
-    "r0" : r0,
-    "r1" : r1,
-    "r2" : r2,
-    "r3" : r3,
-    "b" : b,
-    "sp" : sp,
-    "pc" : pc
+    "r0": r0,
+    "r1": r1,
+    "r2": r2,
+    "r3": r3,
+    "b": b,
+    "sp": sp,
+    "pc": pc
 }
 
 #########################
@@ -34,18 +34,18 @@ registers = {
 #### REGISTER WINDOW ####
 
 reg_positions = {
-    60 : r0,
-    80 : r1,
-    100 : r2,
-    120 : r3,
-    140 : b,
-    160 : sp,
+    60: r0,
+    80: r1,
+    100: r2,
+    120: r3,
+    140: b,
+    160: sp,
     180: pc
- }
+}
 
 width, height = 300, 600
 
-reg_window = pygame.display.set_mode((width,height))
+reg_window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Register Window')
 reg_window.fill((33, 33, 33))
 
@@ -55,18 +55,23 @@ reg_window.blit(title, (20, 20))
 
 text_font = pygame.font.SysFont(None, 24)
 
+
 async def run_console():
     while True:
         inst = await aioconsole.ainput("> ")
+        if inst == "quit":
+            quit()
         for i in parser.parse(tokenize(inst)):
             if i.type == "AddNode":
+                pc.value += 1  # Increment the PC by 1 byte for the instruction
                 left = registers[i.left].value
                 right = registers[i.right].value
                 result = float(left + right)
                 registers[i.left].value = result
-                #TODO: Increment the program counter
-                #print("console", registers)
+                pc.value += 1  # Increment the PC by 1 byte, 4 bits for each register
+                # print("console", registers)
                 await asyncio.sleep(0)
+
 
 async def run_window():
     while True:
@@ -74,21 +79,22 @@ async def run_window():
             if event.type == pygame.QUIT:
                 run = False
         reg_window.fill((33, 33, 33))
-        #reg_window.blit(title, (20, 20))
+        # reg_window.blit(title, (20, 20))
 
         for curr_reg in reg_positions:
-            text = text_font.render(reg_positions[curr_reg].name + " : " + str(reg_positions[curr_reg].value), True, (255, 255, 255))
+            text = text_font.render(reg_positions[curr_reg].name + " : " + str(reg_positions[curr_reg].value), True,
+                                    (255, 255, 255))
             reg_window.blit(text, (20, curr_reg))
         pygame.display.update()
-        #print("window", registers)
+        # print("window", registers)
         await asyncio.sleep(1 / 60)
 
+
 async def main():
-    tasks = []
-    tasks.append(asyncio.create_task(run_console()))
-    tasks.append(asyncio.create_task(run_window()))
+    tasks = [asyncio.create_task(run_console()), asyncio.create_task(run_window())]
 
     for task in tasks:
         await task
+
 
 asyncio.run(main())
